af87f77ca7c4f6b51aef263873cfcf72110163a8
PanZhangg <dazhangpan@gmail.com>
2016-02-03 18:14:32 -0800

    rx_queue & test

--- src/mf_rx_queue.c
+++ src/mf_rx_queue.c
@@ -13,7 +13,6 @@ struct q_node* q_node_init(void* packet_buffer, uint16_t length){
 }
 
 struct mf_rx_queue* mf_rx_queue_init(){
-	printf("--malloc starts---");
 	struct mf_rx_queue* q = (struct mf_rx_queue*)malloc(sizeof(struct mf_rx_queue));
 	if(q == NULL){
 		printf("\nmalloc memory failed\n");
@@ -27,29 +26,25 @@ struct mf_rx_queue* mf_rx_queue_init(){
 }
 
 uint8_t push_q_node(struct q_node* n, struct mf_rx_queue* q){
-	uint8_t success_flag = 0;
 	if(n == NULL || q == NULL)
-		return success_flag;
+		return 0;
 	if(!q->queue_length){
 		q->head = n;
 		q->tail = n;
 		q->queue_length = 1;
-		success_flag = 1;
-		return success_flag;
+		return 1;
 	}else{
 		q->tail->next_node = n;
 		n->previous_node = q->tail;
 		n->next_node = NULL;
 		q->tail = n;
 		q->queue_length++;
-		success_flag = 1;
-		return success_flag;
+		return 1;
 	}
-	return success_flag;
 }
 
 struct q_node* pop_q_node(struct mf_rx_queue* q){
-	if(q->queue_length)
+	if(!q->queue_length)
 		return NULL;
 	else{
 		struct q_node* tmp = NULL;
@@ -68,19 +63,17 @@ void destory_q_node(struct q_node* n){
 }
 
 void destory_queue(struct mf_rx_queue* q){
-	uint64_t i;
-	struct q_node* tmp;
 	while(q->head){
+		struct q_node* tmp;
 		tmp = q->head;
 		q->head = tmp->next_node;
-		free(q->head);
+		free(tmp);
 	}
 	free(q);
 }
 
 static void print_q_node(struct q_node* n){
 	char* content = (char*)(n->rx_packet);
-	content[n->packet_length] = '\0';
 	printf("\nPacket content:%s\n", content);
 }
 
--- tests/queue_test.c
+++ tests/queue_test.c
@@ -19,15 +19,47 @@ static char* test_q_node_init(){
 
 static char* test_push_q_node(){
 	struct mf_rx_queue* queue = mf_rx_queue_init();
-	printf("----here---");
-	struct q_node* node1 = q_node_init(packet_content1, packet_length);
+	struct q_node* node1 = q_node_init(packet_content2, packet_length);
+	//printf("\nPacket_content:%s\n",(char*)(node1->rx_packet));
 	mu_assert(push_q_node(node1, queue) == 1 ,"push queue failed");
+	struct q_node* node2 = q_node_init(packet_content3, packet_length);
+	//printf("\nPacket_content:%s\n",(char*)(node2->rx_packet));
+	mu_assert(push_q_node(node2, queue) == 1 ,"push queue failed");
+	struct q_node* node3 = q_node_init(packet_content1, packet_length);
+	mu_assert(push_q_node(node3, queue) == 1 ,"push queue failed");
 	print_queue(queue);
 	return NULL;
 }
 
-
-
+static char* test_pop_q_node(){
+	struct mf_rx_queue* queue = mf_rx_queue_init();
+	struct q_node* node1 = q_node_init(packet_content2, packet_length);
+	//printf("\nPacket_content:%s\n",(char*)(node1->rx_packet));
+	mu_assert(push_q_node(node1, queue) == 1 ,"push queue failed");
+	struct q_node* node2 = q_node_init(packet_content3, packet_length);
+	//printf("\nPacket_content:%s\n",(char*)(node2->rx_packet));
+	mu_assert(push_q_node(node2, queue) == 1 ,"push queue failed");
+	struct q_node* node3 = q_node_init(packet_content1, packet_length);
+	mu_assert(push_q_node(node3, queue) == 1 ,"push queue failed");
+	struct q_node* pop_node = pop_q_node(queue);
+	mu_assert(pop_node != NULL,"pop queue failed");
+	mu_assert(queue->queue_length == 2, "bad length");
+	print_queue(queue);
+	return NULL;
+}
+static char* test_destory_queue(){
+	struct mf_rx_queue* queue = mf_rx_queue_init();
+	struct q_node* node1 = q_node_init(packet_content2, packet_length);
+	//printf("\nPacket_content:%s\n",(char*)(node1->rx_packet));
+	mu_assert(push_q_node(node1, queue) == 1 ,"push queue failed");
+	struct q_node* node2 = q_node_init(packet_content3, packet_length);
+	//printf("\nPacket_content:%s\n",(char*)(node2->rx_packet));
+	mu_assert(push_q_node(node2, queue) == 1 ,"push queue failed");
+	struct q_node* node3 = q_node_init(packet_content1, packet_length);
+	mu_assert(push_q_node(node3, queue) == 1 ,"push queue failed");
+	print_queue(queue);
+	destory_queue(queue);	
+}
 
 static char* test_handle_flow_mod()
 {
@@ -40,10 +72,12 @@ static char* test_handle_flow_mod()
 
  char *all_tests() 
  {
-    mu_suite_start();
-    mu_run_test(test_q_node_init);
-    mu_run_test(test_push_q_node);
-    return NULL;
+	mu_suite_start();
+	mu_run_test(test_q_node_init);
+	mu_run_test(test_push_q_node);
+	mu_run_test(test_pop_q_node);
+	mu_run_test(test_destory_queue);
+	return NULL;
 }
 
 RUN_TESTS(all_tests);
