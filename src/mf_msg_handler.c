3cc0d712babc9d98afd68f5792c112a722893e96
PanZhangg <dazhangpan@gmail.com>
2016-02-17 02:45:40 -0800

    func prototype

--- src/mf_msg_handler.c
+++ src/mf_msg_handler.c
@@ -1,1 +1,14 @@
 #include "mf_msg_handler.h"
+
+
+
+
+void hello_msg_handler(q_node*);
+void echo_msg_handler(q_node*);
+void packet_in_msg_handler(q_node*);
+void flow_remove_msg_handler(q_node*);
+
+
+void msg_handler_thread_start(msg_handler_func mhf);
+
+
--- src/mf_msg_handler.h
+++ src/mf_msg_handler.h
@@ -3,7 +3,15 @@
 
 #include "mf_rx_queue.h"
 
-:w
+typedef void (*msg_handler_func)(struct q_node*);
+
+void hello_msg_handler(q_node*);
+void echo_msg_handler(q_node*);
+void packet_in_msg_handler(q_node*);
+void flow_remove_msg_handler(q_node*);
+
+
+void msg_handler_thread_start(msg_handler_func mhf);
 
 
 
