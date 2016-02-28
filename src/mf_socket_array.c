bb11b15f7874fb613c91ea24f41fec6b2f5b2085
PanZhangg <dazhangpan@gmail.com>
2016-02-28 22:40:46 +0800

    more

--- src/mf_socket_array.c
+++ src/mf_socket_array.c
@@ -25,17 +25,17 @@ struct mf_socket_array* mf_socket_array_init(){
 }
 
 uint8_t insert_mf_socket_array (struct mf_socket_array_node* n, struct mf_socket_array* q){
-	pthread_mutex_lock(&socket_array_mutex);
+//	pthread_mutex_lock(&socket_array_mutex);
 	if(n == NULL || q == NULL)
 	{	
-		pthread_mutex_unlock(&socket_array_mutex);
+//		pthread_mutex_unlock(&socket_array_mutex);
 		return 0;
 	}
 	if(!q->array_length){
 		q->head = n;
 		q->tail = n;
 		q->array_length = 1;
-		pthread_mutex_unlock(&socket_array_mutex);
+//		pthread_mutex_unlock(&socket_array_mutex);
 		return 1;
 	}else{
 		q->tail->next_node = n;
@@ -44,15 +44,15 @@ uint8_t insert_mf_socket_array (struct mf_socket_array_node* n, struct mf_socket
 		n->next_node = NULL;
 		q->array_length++;
 	}
-	pthread_mutex_unlock(&socket_array_mutex);
+//	pthread_mutex_unlock(&socket_array_mutex);
 	return 1;
 }
 
 uint8_t delete_socket_array_node(int socket_fd, struct mf_socket_array* q){
-	pthread_mutex_lock(&socket_array_mutex);
+//	pthread_mutex_lock(&socket_array_mutex);
 	if(socket_fd < 0 || q == NULL)
 	{
-		pthread_mutex_unlock(&socket_array_mutex);
+//		pthread_mutex_unlock(&socket_array_mutex);
 		return 0;
 	}
 	struct mf_socket_array_node * tmp = q->head;
@@ -79,7 +79,7 @@ uint8_t delete_socket_array_node(int socket_fd, struct mf_socket_array* q){
 		}else{
 			if(tmp == q->tail)
 			{
-				pthread_mutex_unlock(&socket_array_mutex);
+//				pthread_mutex_unlock(&socket_array_mutex);
 				return 0;
 			} //No matched socket_fd
 				//return 0;
@@ -89,24 +89,24 @@ uint8_t delete_socket_array_node(int socket_fd, struct mf_socket_array* q){
 	}
 	q->array_length--;
 	destory_mf_socket(tmp->s);
-	pthread_mutex_unlock(&socket_array_mutex);
+//	pthread_mutex_unlock(&socket_array_mutex);
 	return 1;
 }
 
 struct mf_rx_queue* get_rx_queue(int socket_fd, struct mf_socket_array* q){
 	struct mf_socket_array_node* tmp;
-	pthread_mutex_lock(&socket_array_mutex);
+//	pthread_mutex_lock(&socket_array_mutex);
 	tmp = q->head;
 	while(tmp){
 		if(tmp->s.socket_fd == socket_fd)
 		{
-			pthread_mutex_unlock(&socket_array_mutex);
+//			pthread_mutex_unlock(&socket_array_mutex);
 			return tmp->s.rx_queue;
 		}
 		else
 			tmp = tmp->next_node;
 	}
-	pthread_mutex_unlock(&socket_array_mutex);
+//	pthread_mutex_unlock(&socket_array_mutex);
 	return NULL;
 }
 
