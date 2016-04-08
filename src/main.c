a784f50066d3c5ae67d98a4ca70916f1e15c768e
PanZhangg <dazhangpan@gmail.com>
2016-04-08 03:45:52 -0700

    main.c & Makefile

--- src/main.c
+++ src/main.c
@@ -1,1 +1,16 @@
-#include "mf_socket.h"
+#include "../src/mf_socket.h"
+#include "../src/mf_logger.h"
+#include <malloc.h>
+
+
+int main(int argc, char** argv){
+	mallopt(M_ARENA_MAX, 1);
+	mf_logger_open(mf_default_log_path);
+	mf_write_log("--Test starts here--");
+	uint32_t s = mf_listen_socket_create();
+	mf_socket_bind(s);
+	//char command[60];
+	handle_connection(s);
+	mf_logger_close();
+	return 0;
+}
