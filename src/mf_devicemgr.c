7f2250fc00b1b0c9923738c0c192eab68caa2222
PanZhangg <dazhangpan@gmail.com>
2016-07-11 03:04:03 -0700

    devicemgr update

--- src/mf_devicemgr.c
+++ src/mf_devicemgr.c
@@ -40,43 +40,81 @@ struct mf_switch * get_switch(uint32_t sock)
 
 void add_switch(struct mf_switch* sw)
 {
-	//pthread_mutex_lock(&MF_DEVICE_MGR.devicemgr_mutex);
+	pthread_mutex_lock(&MF_DEVICE_MGR.devicemgr_mutex);
 	MF_DEVICE_MGR.mf_switch_map[sw->sockfd] = sw;
 	MF_DEVICE_MGR.total_switch_number++;
-	//pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
+	pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
 }
 
 void delete_switch_from_map(struct mf_switch * sw)
 {
-	//pthread_mutex_lock(&MF_DEVICE_MGR.devicemgr_mutex);
+	pthread_mutex_lock(&MF_DEVICE_MGR.devicemgr_mutex);
 	MF_DEVICE_MGR.mf_switch_map[sw->sockfd] = NULL;
 	MF_DEVICE_MGR.total_switch_number--;
-	//pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
+	pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
 }
+/*The upper limit of agrument loop_index
+is decided by the MAX_SWITCH_NUM
 
+e.g.
+int i = 0; //where 0 is the start index, could be any other value less than MAX_SWITCH_NUM
+
+struct mf_switch * sw = get_next_switch(&i);
+
+returns the next valid sw pointer after i
+also changes i to the this new location of index 
+*/
 struct mf_switch * get_next_switch(uint32_t* loop_index)
 {
-	static uint32_t tmp;
-	if(*loop_index == 0)
-		tmp = 0;
-	while(MF_DEVICE_MGR.mf_switch_map[tmp] == NULL)
-		tmp++;
-	return MF_DEVICE_MGR.mf_switch_map[tmp++];
+	if(*loop_index < 0 || *loop_index >= MAX_MF_SWITCH_NUM)
+	{
+		perror("Bad loop_index");
+		return NULL;
+	}
+	pthread_mutex_lock(&MF_DEVICE_MGR.devicemgr_mutex);
+	for(; *loop_index < MAX_MF_SWITCH_NUM; (*loop_index)++)
+	{ 
+		if(MF_DEVICE_MGR.mf_switch_map[*loop_index] != NULL)
+		{
+			if(*loop_index < MAX_MF_SWITCH_NUM - 1)
+			{
+				(*loop_index)++; 
+				pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
+				return MF_DEVICE_MGR.mf_switch_map[*loop_index - 1];
+			}
+			else
+			{
+				pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
+				return MF_DEVICE_MGR.mf_switch_map[*loop_index];
+			}
+		}
+	}
+	perror("No valid switch");
+	pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
+	return NULL;
 }
 
 struct mf_switch * get_switch_by_dpid(uint64_t dpid)
 {
-	uint32_t i;
+	uint32_t i,curr_index;
+	curr_index = 0;
 	pthread_mutex_lock(&MF_DEVICE_MGR.devicemgr_mutex);
 	for(i = 0; i < MF_DEVICE_MGR.total_switch_number; i++)
 	{
-		struct mf_switch* sw = get_next_switch(&i);
-		if(sw->datapath_id == dpid)
+		struct mf_switch* sw = get_next_switch(&curr_index);
+		if(sw == NULL)
+		{
+			perror("No switch has this dpid");
+			pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
+			return NULL;
+		}
+		else if(sw->datapath_id == dpid)
 		{
 			pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
 			return sw;
 		}
 	}
+	perror("No switch has this dpid");
 	pthread_mutex_unlock(&MF_DEVICE_MGR.devicemgr_mutex);
 	return NULL;
 }
@@ -85,8 +123,8 @@ struct mf_switch * get_switch_by_dpid(uint64_t dpid)
 static uint8_t is_struct_hash_value_identical(struct host_hash_value* a, struct host_hash_value* b)
 {
 	if(a->mac_addr == b->mac_addr \
-		&& a->sw == b->sw \
-		&& a->port_num == b->port_num)
+			&& a->sw == b->sw \
+			&& a->port_num == b->port_num)
 		return 1;
 	else
 		return 0;
@@ -220,7 +258,7 @@ void host_add_to_hash_map(struct host_hash_value* value)
 			Delete hash value structure which hash identical mac addr but different
 			sw or port_num from the global hash map
 			It happens when the same host connect to another port or switch
-			*/
+			 */
 			if(tmp->hash_next)
 				tmp = tmp->hash_next;
 			else
@@ -262,6 +300,8 @@ void host_hash_value_destory(struct host_hash_value* value)
 {
 	if(value)
 		free(value);
+	else
+		perror("value is NULL");
 }
 
 
